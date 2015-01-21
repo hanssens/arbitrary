c58f763ce552f46c40f1411221cb1289ee25d23d
hanssens <github@hanssens.com>
2015-01-21 22:49:34 +0100

    Initial commit of Arbitrary prototype
    - Setup the basic project structure, implemented basic functionality and added some tests
    - Not sure about the naming conventions, though

---
+++ source/Arbitrary/Collections/Names.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arbitrary.Collections
+{
+
+    public class Name : IArbitraryEntity<Name>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public IEnumerable<Name> All()
+        {
+            return new List<Name>()
+            {
+                new Name(){ FirstName = "Harry", LastName = "Potter"},
+                new Name(){ FirstName = "Hermoine", LastName = "Granger"},
+            };
+        }
+    }
+}
